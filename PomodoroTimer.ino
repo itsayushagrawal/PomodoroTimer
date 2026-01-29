9d5a49d140d1a13c4f6dfdb08414ede62a83cd73
Ayush Agrawal <agaayush15@gmail.com>
2026-01-29 22:55:11 +0530

    Get raw acc. data from MPU

--- PomodoroTimer.ino
+++ PomodoroTimer.ino
@@ -1,9 +1,25 @@
-void setup() 
-{
+#include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
+MPU6050 mpu;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+
+  mpu.initialize();
+
+  Serial.println(mpu.testConnection() ? "MPU Connected" : "MPU Not Connected");
 }
 
-void loop() 
-{
+void loop() {
+  int16_t ax, ay, az;
+  mpu.getAcceleration(&ax, &ay, &az);
 
+  Serial.print(ax); Serial.print(" ");
+  Serial.print(ay); Serial.print(" ");
+  Serial.println(az);
+
+  delay(500);
 }
