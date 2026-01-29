0109ae94177ede5f2b026864d6f203913b6ac9f8
Ayush Agrawal <agaayush15@gmail.com>
2026-01-29 22:57:24 +0530

    Convert to g values

--- PomodoroTimer.ino
+++ PomodoroTimer.ino
@@ -17,9 +17,13 @@ void loop() {
   int16_t ax, ay, az;
   mpu.getAcceleration(&ax, &ay, &az);
 
-  Serial.print(ax); Serial.print(" ");
-  Serial.print(ay); Serial.print(" ");
-  Serial.println(az);
+  float ax_g = ax / 16384.0;      //Convert raw data into g values
+  float ay_g = ay / 16384.0;
+  float az_g = az / 16384.0;
+
+  Serial.print(ax_g); Serial.print(" ");
+  Serial.print(ay_g); Serial.print(" ");
+  Serial.println(az_g);
 
   delay(500);
 }
